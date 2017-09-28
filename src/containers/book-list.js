b5eea18c77ae052b3eb585cffa164199184fcb7f
Merilee Wheelock <eelirem@gmail.com>
2017-09-28 14:14:35 -0700

    add actions

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index.js';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component{
 
@@ -27,4 +29,8 @@ function mapStateToProps(state){
 	}
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
